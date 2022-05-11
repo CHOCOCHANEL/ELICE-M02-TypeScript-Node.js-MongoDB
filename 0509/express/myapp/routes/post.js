9557805f724b6488637273a3cbf437baec48bb76
CHOCO <jh780465@gmail.com>
2022-05-11 14:00:44 +0900

    my first mongo DB PUT success!

--- 0509/express/myapp/routes/post.js
+++ 0509/express/myapp/routes/post.js
@@ -1,11 +1,12 @@
 var express = require("express");
 var router = express.Router();
+var bookSchema = require("../models/book");
 
 router.get('/', function(req, res) {
     res.render('post', { title: 'Example-Post' });
   });
 
-router.post('/', (req, res, next)=>{
+router.post('/', (req, res)=>{
     const name = req.body.name;
     const phone = req.body.phone;
     const date = req.body.date;
@@ -14,6 +15,23 @@ router.post('/', (req, res, next)=>{
         phone: phone,
         date: date,
     });
+});
+
+router.post('/addbook', (req, res)=>{
+    const bookName = req.body.bookName;
+    const author = req.body.author;
+    const price = req.body.price;
+    const publish = req.body.publish;
+    
+    const bookData = new bookSchema({
+        bookName: bookName,
+        author: author,
+        price: price,
+        publish: publish
+    })
+
+    bookData.save();
+    res.redirect('/example');
 });
 
 module.exports = router;
