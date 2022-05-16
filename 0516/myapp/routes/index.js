8eca7deaa8fd5f35c66f1afb517457bbfd912a3c
CHOCO <jh780465@naver.com>
2022-05-16 10:31:10 +0900

    Controller Initial Commit

--- 0516/myapp/routes/index.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const mainController = require('../controller/main');
-
-/* GET home page. */
-router.get('/', mainController );
-
-module.exports = router;
