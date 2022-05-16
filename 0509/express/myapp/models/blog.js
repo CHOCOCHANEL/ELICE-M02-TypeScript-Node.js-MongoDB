59ab8d9e9dddc25d261056d45fc91620c157b1dc
CHOCO <jh780465@naver.com>
2022-05-16 14:58:43 +0900

    Mongo DB AutoIncrement to Blog posting added

--- 0509/express/myapp/models/blog.js
+++ 0509/express/myapp/models/blog.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const autoIncrement = require('mongoose-auto-increment');
+
+autoIncrement.initialize(mongoose);
 
 const blogSchema = new Schema({
     title: {
@@ -11,8 +14,16 @@ const blogSchema = new Schema({
         type: String,
         required: true,
     },
+    no: Number,
 }, {
     timestamps: true,
 })
+
+blogSchema.plugin(autoIncrement.plugin, {
+    model: 'blog',
+    field: 'no',
+    startAt: 1,
+    increment: 1,
+});
 
 module.exports = mongoose.model('blog', blogSchema);;
