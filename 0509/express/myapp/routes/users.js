0bc53648e3813e19c9bbc9e80b49518c131feae7
CHOCO <jh780465@naver.com>
2022-05-18 14:18:36 +0900

    Asynchoronous Error Handler added

--- 0509/express/myapp/routes/users.js
+++ 0509/express/myapp/routes/users.js
@@ -4,12 +4,13 @@ const { renderController,
         signupController,
         signinController} = require('../controller/user');
 const { body, validationResult } = require('express-validator');
+const asyncErrorHandler = require('../utils/async-handler');
 
 /* GET users listing. */
-router.get('/', renderController);
+router.get('/', asyncErrorHandler(renderController));
 router.post('/signup', body('email').isEmail().withMessage('Wrong Email Format'), 
                        body('password').isLength({ min: 5 }).withMessage('Wrong Paswword Format'), 
-                       signupController);
-router.post('/signin', signinController);
+                       asyncErrorHandler(signupController));
+router.post('/signin', asyncErrorHandler(signinController));
 
 module.exports = router;
