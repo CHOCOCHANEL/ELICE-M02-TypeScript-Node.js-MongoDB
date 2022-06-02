36f4673c6155970e7e1f7b182cc6eda37149b095
CHOCOCHANEL <jh780465@naver.com>
2022-06-02 17:35:31 +0900

    wrote all but app crashed for some reason

--- 0602/signup/app.js
+++ 0602/signup/app.js
@@ -3,15 +3,23 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
+var dayjs = require('dayjs');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var postsRouter = require('./routes/posts');
+
+var dbconnect = require('./models/index');
+dbconnect();
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+app.locals.formatDate = (date) => {
+  return dayjs(date).format('YYYY-MM-DD HH:mm:ss');
+}
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -20,7 +28,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/posts', postsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
