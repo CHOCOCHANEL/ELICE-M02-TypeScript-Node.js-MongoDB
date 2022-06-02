f80c3ce1b9e26bfdb4a3f464c39361e8ff7382bd
CHOCOCHANEL <jh780465@naver.com>
2022-06-02 17:25:56 +0900

    models revised

--- 0602/signup/models/index.js
+++ 0602/signup/models/index.js
@@ -1,11 +1,45 @@
 const mongoose = require('mongoose');
 const PostSchema = require('./schemas/post');
 const UserSchema = require('./schemas/user');
-
+const connect = require('../dbconfig');
+const url = 'mongodb://' + connect.username + ':' + connect.password
+    + '@' + connect.url + '/' + connect.dbname;
 const Post = mongoose.model('Post', PostSchema);
 const User = mongoose.model('User', UserSchema);
 
-module.exports = {
-    Post,
-    User,
+const dbconnect = () => {
+    mongoose.connect(
+        url,
+        error => {
+            if (error) {
+                console.log("mongodb connect error", error);
+            } else {
+                console.log("mongodb-connect-success");
+            }
+        }
+    );
 };
+
+mongoose.connection.on('error', error => {
+    console.log('mongodb connection error', error);
+});
+
+mongoose.connection.on('disconnected', () => {
+    console.error('mongodb disconnected try reconnect....');
+});
+
+async function boot() {
+    for (let i = 0; i < 100; i++) {
+      await Post.create({
+        title: `게시글 ${i} 번`,
+        content: `게시글 ${i} 번 내용`,
+      });
+    }
+  }
+  
+boot()
+    .then(() => process.exit());
+
+module.exports = dbconnect;
+module.exports = Post;
+module.exports = User;
